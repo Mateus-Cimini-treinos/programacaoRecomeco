f03f1a475539b08cc8d492291eb7c7ce6325cd7b
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2025-05-20 00:54:33 -0300

    chore: adicionando exercicio sobre funçoes

---
+++ modulo06JavaScript/aula07Funcoes/execicio.js
@@ -0,0 +1,10 @@
+// crie uma funçao que receba a altura e a base e deve retonar a area
+
+function calculaArea(altura = 0, base = 0) {
+    const area = altura * base;
+    return area;
+}
+
+console.log(`A area é de : ${calculaArea(4, 2)}`);
+
+
